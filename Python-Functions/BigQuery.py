ba3a86279a4b233511b3e47cde954f5a76322778
bhereth <bhereth@wfrc.org>
2023-07-07 13:02:18 -0600

    Add info for bill's home mac

--- Python-Functions/BigQuery.py
+++ Python-Functions/BigQuery.py
@@ -9,11 +9,13 @@ def getBigQueryClient_TDMScenarios():
 
     if os.getlogin()=='cday':
         key_path = r"C:\Users\cday\tdm-scenarios-c90ba30c3c5d.json"
-    elif os.getlogin()=='bhereth':
+    elif os.getlogin()=='bhereth': 
         key_path = r"C:\Users\bhereth\tdm-scenarios-a85044dbbfd3.json"
+    # add mac user name
+    elif os.environ['USER']=='William': 
+        key_path = os.path.expanduser("~/tdm-scenarios-a85044dbbfd3.json")
     elif os.getlogin()=='sswim':
         key_path = r"C:\Users\sswim\tdm-scenarios-a5ce45fae4b0.json"
-
     credentials = service_account.Credentials.from_service_account_file(
         filename = key_path, scopes=["https://www.googleapis.com/auth/cloud-platform"],
     )
@@ -26,8 +28,11 @@ def getBigQueryClient_StreetLight():
 
     if os.getlogin()=='Cday':
         key_path = r""
-    elif os.getlogin()=='bhereth':
+    elif os.getlogin()=='bhereth': # add mac user name
         key_path = r"C:\Users\bhereth\API Keys\streetlight-temp-analysis-e2b201d26862.json"
+    # add mac user name
+    elif os.environ['USER']=='William': 
+        key_path = os.path.expanduser("~/streetlight-temp-analysis-e2b201d26862")
     elif os.getlogin()=='sswim':
         key_path = r""
 
