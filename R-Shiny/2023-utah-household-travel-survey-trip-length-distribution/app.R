4470110d14111193cf6b90dac9adbe6ba0793de8
Bill Hereth <bhereth@wfrc.org>
2024-05-09 13:47:44 -0600

    Implement WF area of interest and loading prompt

--- R-Shiny/2023-utah-household-travel-survey-trip-length-distribution/app.R
+++ R-Shiny/2023-utah-household-travel-survey-trip-length-distribution/app.R
@@ -25,11 +25,14 @@ ui <- dashboardPage(
     )
   ),
   dashboardSidebar(
-    selectInput("groupSampleSegmentLabel", "Survey Sample Segment Group:",
-                choices = setNames(labelsSampleSegmentGroups$value, labelsSampleSegmentGroups$label)),
-    selectInput("groupModeTypeBroad", "Broad Mode:",
+    radioButtons("segmentSelect", "Choose Geography:",
+                 choices = list("Sample Segment Group" = "sampleSegment",
+                                "Area of Interest" = "areaInterest"),
+                 selected = "sampleSegment"),
+    uiOutput("dynamicSelect"),
+    selectInput("groupModeTypeBroad", "Travel Mode:",
                 choices = setNames(labelsModeTypeBroad$value, labelsModeTypeBroad$label)),
-    selectInput("groupTripType", "Trip Type:",
+    selectInput("groupTripType", "Trip Purpose:",
                 choices = setNames(labelsTripType$value, labelsTripType$label)),
     selectInput("groupNumVehicles", "Household Number of Vehicles:",
                 choices = setNames(labelsNumVehicles$value, labelsNumVehicles$label)),
@@ -44,6 +47,7 @@ ui <- dashboardPage(
                 selected = 30)
   ),
   dashboardBody(
+    uiOutput("loadingMessage"),
     tags$head(
       tags$style(HTML("
         .shiny-output-error-validation { color: red; }
@@ -75,10 +79,7 @@ ui <- dashboardPage(
     )
   )
 )
-
-# Define server logic
 server <- function(input, output, session) {
-  
   # Display the alert when the app is opened
   shinyalert(
     text ="The 2023 Utah Moves household travel survey was designed and conducted for use in regional and statewide travel demand modeling. The sample size and frame is suitable for that purpose.
@@ -90,15 +91,50 @@ server <- function(input, output, session) {
     confirmButtonCol = 'navy'
   )
   
+  output$loadingMessage <- renderUI({
+    if (is.null(selectedSampleSegment())) {
+      div(style = "color: red; font-weight: bold;", "Please wait while data loads...")
+    } else {
+      NULL  # No message if the condition is met
+    }
+  })
+  
+  # Define reactive values
+  selectedSampleSegment <- reactiveVal()
+  filterTextSampleSegment <- reactiveVal()
+  
+  # Render UI based on user selection
+  output$dynamicSelect <- renderUI({
+    if(input$segmentSelect == "sampleSegment") {
+      selectInput("groupSampleSegment", "Sample Segment Group:",
+                  choices = setNames(labelsSampleSegmentGroups$value, labelsSampleSegmentGroups$label))
+    } else {
+      selectInput("areaInterest", "Area of Interest:",
+                  choices = setNames(labelsAreaInterest$value, labelsAreaInterest$label))
+    }
+  })
+  
+  # Observe changes in segment selection
+  observeEvent(input$groupSampleSegment, {
+    selectedSampleSegment(input$groupSampleSegment)
+    filterTextSampleSegment(paste("Sample Segment: ", labelsSampleSegmentGroups$label[labelsSampleSegmentGroups$value == input$groupSampleSegment]))
+  })
+  
+  observeEvent(input$areaInterest, {
+    selectedSampleSegment(input$areaInterest)
+    filterTextSampleSegment(paste("Area of Interest: ", labelsAreaInterest$label[labelsAreaInterest$value == input$areaInterest]))
+  })
+  
+  
   # Handle the methodology modal dialog
   observeEvent(input$showMethodology, {
     showModal(modalDialog(
       title = "Methodology",
-      tags$p("The trip-length distributions from household travel survey was data was processed using the following steps:"),
+      tags$p("The trip-length distributions were generated from the trip distance field in the trip table as recorded in the Household Travel Survey using the following steps:"),
       tags$p(tags$b("Prepare distance bins")),
       tags$p("A view was created that contains starting bin values for bin sizes of 1/2 mile, 1 mile and 5 miles from 0 to 100 using QUERY 1."),
       tags$p(tags$b("Prepare grouping tables")),
-      tags$p("Five separate views were created to further group values from 5 key dimensions to the data: sample segment (geography and population), household number of workers, household number of vehicles, trip broad mode type, and trip type (purpose). The only grouping added to the current survey breakdown was an 'All' group that includes all possible values for each respective field. The 'All' group uses an attribute value of -1 to not overlap with existing attribute values. An example SQL for number of vehicles is shown in QUERY 2."),
+      tags$p("Five separate views were created to further group values from 5 key dimensions to the data: sample segment (geography and population), household number of workers, household number of vehicles, trip travel mode type, and trip type (purpose). The only grouping added to the current survey breakdown was an 'All' group that includes all possible values for each respective field. The 'All' group uses an attribute value of -1 to not overlap with existing attribute values. An example SQL for number of vehicles is shown in QUERY 2."),
       tags$p(tags$b("Prepare main query")),
       tags$p("The main query was created using the household table and the trip table joined on the hh_id field. The view for distance bins is included with a condition that the trip.distance_miles is greater than or equal to the binStart value and less than binStart + binSize. The five views for groupings are also added by using key values to link the tables. The trip_weight and number of records is aggregated for each combination of bin size and key dimension using their respective group fields. Resulting record counts in the numTripRecords field were used to judge rough accuracy of the query structure. See QUERY 3."),
       tags$p(tags$b("Calculate histogram distribution and cumulative distribution")),
@@ -110,26 +146,39 @@ server <- function(input, output, session) {
 <pre style="background-color: #f5f5f5; padding: 10px; border-radius: 5px; border: 1px solid #ccc; font-family: monospace;">
   <code>
 -- Create list of numbers that will be filtered below by MOD function
-WITH RECURSIVE NumberSeries AS (
-  SELECT 0 AS num
-  UNION ALL
-  SELECT num + 5 FROM NumberSeries WHERE num < 1000
+-- Generate a series of numbers from 0 to 995, incrementing by 5
+WITH NumberSeries AS (
+  SELECT num
+  FROM UNNEST(GENERATE_ARRAY(0, 995, 5)) AS num
 )
 
--- Half-mile bin sizes up to 100
-SELECT 0.5 AS binSize, num / 10.0 AS binStart FROM NumberSeries WHERE MOD(num, 5) = 0 AND num / 10.0 < 100
+-- Half-mile bin size
+SELECT
+  0.5 AS binSize,
+  num / 10.0 AS binStart
+FROM NumberSeries
+WHERE MOD(num, 5) = 0 AND num / 10.0 < 100
 
 UNION ALL
 
--- 1-mile bin sizes up to 100
-SELECT 1.0 AS binSize, num / 10.0 AS binStart FROM NumberSeries WHERE MOD(num, 10) = 0 AND num / 10.0 < 100
+-- 1-mile bin size
+SELECT
+  1.0 AS binSize,
+  num / 10.0 AS binStart
+FROM NumberSeries
+WHERE MOD(num, 10) = 0 AND num / 10.0 < 100
 
 UNION ALL
 
--- 5 mile bin sizes up to 100
-SELECT 5.0 AS binSize, num / 10.0 AS binStart FROM NumberSeries WHERE MOD(num, 50) = 0 AND num / 10.0 < 100
+-- 5-mile bin size
+SELECT
+  5.0 AS binSize,
+  num / 10.0 AS binStart
+FROM NumberSeries
+WHERE MOD(num, 50) = 0 AND num / 10.0 < 100
 
-ORDER BY binSize, binStart
+ORDER BY
+  binSize, binStart;
   </code>
 </pre>
         ')
@@ -226,10 +275,16 @@ observe({
   numeric_binSize <- as.numeric(input$binSize)
   numeric_maxX <- as.numeric(input$maxX)
   
+  # Handle plot rendering
   output$dataPlot <- renderPlot({
+    req(selectedSampleSegment())  # Ensure data is selected before plotting
+
+    # Simulate data retrieval and processing
+    Sys.sleep(2)  # Simulate data processing time
+    
     # Placeholder data processing; actual logic should match your data structure and needs
     filtered_data <- data %>%
-      filter(groupSampleSegmentLabel == input$groupSampleSegmentLabel,
+      filter(groupSampleSegment == selectedSampleSegment(),
              groupNumWorkers == input$groupNumWorkers,
              groupNumVehicles == input$groupNumVehicles,
              groupTripType == input$groupTripType,
@@ -265,10 +320,11 @@ observe({
     }
   })
   output$selectedInputs <- renderText({
+    req(filterTextSampleSegment())  # Ensure data is selected before plotting
     paste("\n",
           "Current Filter:",
-          paste0("\u00A0\u00A0\u00A0Sample Segment Group: ", labelsSampleSegmentGroups$label[labelsSampleSegmentGroups$value == input$groupSampleSegmentLabel]),
-          paste0("\u00A0\u00A0\u00A0Broad Mode: ", labelsModeTypeBroad$label[labelsModeTypeBroad$value == input$groupModeTypeBroad]),
+          paste0("\u00A0\u00A0\u00A0", filterTextSampleSegment()),
+          paste0("\u00A0\u00A0\u00A0Travel Mode: ", labelsModeTypeBroad$label[labelsModeTypeBroad$value == input$groupModeTypeBroad]),
           paste0("\u00A0\u00A0\u00A0Trip Type: ", labelsTripType$label[labelsTripType$value == input$groupTripType]),
           paste0("\u00A0\u00A0\u00A0Number of Vehicles: ", labelsNumVehicles$label[labelsNumVehicles$value == input$groupNumVehicles]),
           paste0("\u00A0\u00A0\u00A0Number of Workers: ", labelsNumWorkers$label[labelsNumWorkers$value == input$groupNumWorkers]),
@@ -278,9 +334,11 @@ observe({
   
   
   output$dataTable <- renderTable({
+    req(selectedSampleSegment())  # Ensure data is selected before plotting
+    
     # Placeholder for your data table logic
     filtered_data <- data %>%
-      filter(groupSampleSegmentLabel == input$groupSampleSegmentLabel,
+      filter(groupSampleSegment == selectedSampleSegment(),
              groupNumWorkers == input$groupNumWorkers,
              groupNumVehicles == input$groupNumVehicles,
              groupTripType == input$groupTripType,
